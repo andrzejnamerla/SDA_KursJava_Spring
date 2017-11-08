3ba8843b07f5b3399acfd3db53df9f2fccb8cae4
andrzejnamerla <andrzej.namerla@gmail.com>
2017-11-08 19:11:35 +0100

    dodana h2 database dodane UsersRepository

--- src/main/java/com/sda/springjavapoz4/service/UsersService.java
+++ src/main/java/com/sda/springjavapoz4/service/UsersService.java
@@ -1,6 +1,7 @@
 package com.sda.springjavapoz4.service;
 
 import com.sda.springjavapoz4.model.User;
+import com.sda.springjavapoz4.repository.UsersRepository;
 import com.sda.springjavapoz4.service.generator.FirstNameGenerator;
 import com.sda.springjavapoz4.service.generator.LastNameGenerator;
 import com.sda.springjavapoz4.service.generator.PhoneNumberGenerator;
@@ -8,15 +9,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Component
 public class UsersService {
 
-    private List<User> users;
+    //private List<User> users;
+
+    @Autowired
+    private UsersRepository usersRepository;
 
     @Autowired
     private FirstNameGenerator firstNameGenerator;
@@ -28,35 +31,42 @@ public class UsersService {
     private PhoneNumberGenerator phoneNumberGenerator;
 
     public UsersService() {
-        this.users = new ArrayList<>();
+        //this.users = new ArrayList<>();
     }
 
     @PostConstruct
     public void init() {
-        this.users.add(getExampleUser());
-        this.users.add(getExampleUser());
-        this.users.add(getExampleUser());
-        this.users.add(getExampleUser());
-        this.users.add(getExampleUser());
-        this.users.add(getExampleUser());
-        this.users.add(getExampleUser());
-        this.users.add(getExampleUser());
-        this.users.add(getExampleUser());
-        this.users.add(getExampleUser());
+        usersRepository.save(getExampleUser());
+        usersRepository.save(getExampleUser());
+        usersRepository.save(getExampleUser());
+        usersRepository.save(getExampleUser());
+        usersRepository.save(getExampleUser());
+        usersRepository.save(getExampleUser());
+        usersRepository.save(getExampleUser());
+        usersRepository.save(getExampleUser());
+        usersRepository.save(getExampleUser());
+        usersRepository.save(getExampleUser());
 
-        users.forEach(user -> System.out.println(user));
+        usersRepository.findAll().forEach(user -> System.out.println(user));
+    }
+
+    public User getRandomUser(){
+        return getExampleUser();
+
+    }
+
+    public User getExampleUser() {
+        User user = new User(1, firstNameGenerator.getRandomFirstName(), lastNameGenerator.getRandomLastName(), phoneNumberGenerator.getPhoneNumberName());
+        return user;
     }
 
     public User getUser(int id) {
-        if (id <= users.size()) {
-            users.get(id).setId(id);
-        }
-        return id >= users.size() ? null : users.get(id);
+        return usersRepository.findOne((long) id);
     }
 
     public List<User> getUserByFirstName(String firstName){
 
-        return users.stream()
+        return StreamSupport.stream(usersRepository.findAll().spliterator(),false)
                 .filter(user -> user.getFirstName().equals(firstName))
                 .collect(Collectors.toList());
 
@@ -69,23 +79,12 @@ public class UsersService {
         return userList;*/
     }
 
-    public User getRandomUser(){
-        Random random = new Random();
-        int randomIndex = random.nextInt(users.size());
-        return users.get(randomIndex);
-
-    }
-
-    public User getExampleUser() {
-        User user = new User(1, firstNameGenerator.getRandomFirstName(), lastNameGenerator.getRandomLastName(), phoneNumberGenerator.getPhoneNumberName());
-        return user;
-    }
-
     public List<User> getAllUsers() {
-        return users;
+        return StreamSupport.stream(usersRepository.findAll().spliterator(), false)
+                .collect(Collectors.toList());
     }
 
     public void addUser(User user){
-        users.add(user);
+        usersRepository.save(user);
     }
 }
